6e576a7bc0ff0d07dd18f41a5b8d29851ddfefb4
Lydia <lscchen1990@gmail.com>
2016-11-01 16:36:10 -0500

    Lydia Roman Numberals if statements up to 20

--- src/lydia/main/com/usaa/g6/lydia/RomanNumeralConverter.java
+++ src/lydia/main/com/usaa/g6/lydia/RomanNumeralConverter.java
@@ -5,12 +5,37 @@ package com.usaa.g6.lydia;
  */
 public class RomanNumeralConverter {
     public String convert(int value) {
-        if (value==1) {
-            return "I";
-        } else if (value==2) {
-            return "II";
-        } else {
-            return "III";
+        String result="";
+
+        if (value/10!=0){
+            int tens = value/10;
+            for (int x=0;x<tens;x++) {
+                result+= "X";
+            }
         }
+
+        if ( value/5==1 || (value+1)/5==1 ) {
+            result += "V";
+        }
+
+        if (value%5==4){
+            if (value<10){
+                value = (value+1)/5;
+                result = "I" + result;
+                value = value*5-1;
+            }
+            if (value>=10){
+                result = result + "I" + result;
+            }
+        }
+
+        if (value%5!=0 && value%5!=4){
+            for (int i =0;i<value%5;i++){
+                result+= "I";
+            }
+        }
+
+
+        return result;
     }
 }
--- src/lydia/test/com/usaa/g6/lydia/RomanNumeralConverterTest.java
+++ src/lydia/test/com/usaa/g6/lydia/RomanNumeralConverterTest.java
@@ -21,4 +21,49 @@ public class RomanNumeralConverterTest {
     public void testRomanConvertOfArabicNumeral3ToIII() {
         assertEquals("III", new RomanNumeralConverter().convert(3));
     }
+
+    @Test
+    public void testRomanConvertOfArabicNumeral4ToIV() {
+        assertEquals("IV", new RomanNumeralConverter().convert(4));
+    }
+
+    @Test
+    public void testRomanConvertOfArabicNumeral5ToV() {
+        assertEquals("V", new RomanNumeralConverter().convert(5));
+    }
+
+    @Test
+    public void testRomanConvertOfArabicNumeral6ToVI() {
+        assertEquals("VI", new RomanNumeralConverter().convert(6));
+    }
+
+    @Test
+    public void testRomanConvertOfArabicNumeral7ToVII() {
+        assertEquals("VII", new RomanNumeralConverter().convert(7));
+    }
+
+    @Test
+    public void testRomanConvertOfArabicNumeral9ToIV() {
+        assertEquals("IV", new RomanNumeralConverter().convert(9));
+    }
+
+    @Test
+    public void testRomanConvertOfArabicNumeral10ToX() {
+        assertEquals("X", new RomanNumeralConverter().convert(10));
+    }
+
+    @Test
+    public void testRomanConvertOfArabicNumeral19ToXIX() {
+        assertEquals("XIX", new RomanNumeralConverter().convert(19));
+    }
+
+    @Test
+    public void testRomanConvertOfArabicNumeral20ToXX() {
+        assertEquals("XX", new RomanNumeralConverter().convert(20));
+    }
+
+    @Test
+    public void testRomanConvertOfArabicNumeral22ToXXII() {
+        assertEquals("XXII", new RomanNumeralConverter().convert(22));
+    }
 }

50278db3d0e10114a35acc5d21f14b52f2ee055b
zangrum <rwwise@gmail.com>
2016-11-01 09:36:26 -0500

    Adding the hamcrest lib

---
+++ src/rick/main/com/usaa/g6/rick/RomanNumeralConverter.java
@@ -0,0 +1,7 @@
+package com.usaa.g6.rick;
+
+/**
+ * Created by IT93168 on 11/1/2016.
+ */
+public class RomanNumeralConverter {
+}
---
+++ src/rick/test/com/usaa/g6/rick/RomanNumeralConverterTest.java
@@ -0,0 +1,14 @@
+package com.usaa.g6.rick;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by IT93168 on 11/1/2016.
+ */
+public class RomanNumeralConverterTest {
+
+    @Test
+    public void testRomanConvertOfArabicNumeral1ToI(){
+        fail("Not Yet Implemented");
+    }
+}

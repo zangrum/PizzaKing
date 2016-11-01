1056a78cbe917e7c47e09b54b635f84ace5e74cc
zangrum <rwwise@gmail.com>
2016-11-01 10:53:06 -0500

    First test working 1 to I

--- src/rick/main/com/usaa/g6/rick/RomanNumeralConverter.java
+++ src/rick/main/com/usaa/g6/rick/RomanNumeralConverter.java
@@ -4,4 +4,7 @@ package com.usaa.g6.rick;
  * Created by IT93168 on 11/1/2016.
  */
 public class RomanNumeralConverter {
+    public String convert(int value) {
+        return "I";
+    }
 }
--- src/rick/test/com/usaa/g6/rick/RomanNumeralConverterTest.java
+++ src/rick/test/com/usaa/g6/rick/RomanNumeralConverterTest.java
@@ -1,14 +1,16 @@
 package com.usaa.g6.rick;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 /**
  * Created by IT93168 on 11/1/2016.
  */
 public class RomanNumeralConverterTest {
 
     @Test
-    public void testRomanConvertOfArabicNumeral1ToI(){
-        fail("Not Yet Implemented");
+    public void testRomanConvertOfArabicNumeral1ToI() {
+        assertEquals("I", new RomanNumeralConverter().convert(1));
     }
 }

68f44d05c3982dbe10f09c94c27701e54c939815
sydneynotthecity <sydney.z.condie@gmail.com>
2016-11-01 11:03:26 -0500

    Checking in Junit Library

---
+++ src/syd/main/com/usaa/g6/syd/RomanNumeralConverter.java
@@ -0,0 +1,29 @@
+package com.usaa.g6.syd;
+
+/**
+ * Created by sydneycondie on 11/1/16.
+ */
+public class RomanNumeralConverter {
+    public String convert(int value) {
+        //return "I";
+        for (int i = i; i <= value; i++){
+            String romanNumeral = "I";
+            return romanNumeral;
+            romanNumeral = romanNumeral + "I";
+        }
+        //if (value == 1) {
+        //    return "I";
+        //}
+        //else if (value == 2) {
+        //    return "II";
+        //}
+        //else if (value == 3){
+        //    return "III";
+        //}
+        //else {
+        //    return "Null";
+        //}
+
+    }
+}
+
